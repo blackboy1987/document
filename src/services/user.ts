54000c514582a41675e5fa098d87e89714d6a1e0
1169794338@qq.com <1169794338@qq.com>
2020-05-20 16:02:37 +0800

    init

--- src/services/user.ts
+++ src/services/user.ts
@@ -1,11 +1,14 @@
 import request from '@/utils/request';
+import constants from '@/utils/constants';
 
 export async function query(): Promise<any> {
   return request('/api/users');
 }
 
 export async function queryCurrent(): Promise<any> {
-  return request('/api/currentUser');
+  return request(`${constants.memberUrl}currentUser`, {
+    method: 'POST',
+  });
 }
 
 export async function queryNotices(): Promise<any> {
